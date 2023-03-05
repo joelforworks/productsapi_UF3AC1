723df2835235976238f82a7adcbb255d5bef3be9
joelforworks <101220133+joelforworks@users.noreply.github.com>
2023-03-05 22:36:00 +0100

    env

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log(process.env)
 const express = require('express')
 const products_routes = require('./routes/products.js')
 const slugify = require('slugify');
