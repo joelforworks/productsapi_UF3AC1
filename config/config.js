a83f38e977d72a6e9b6421673a3cffaa30f0de14
joelforworks <101220133+joelforworks@users.noreply.github.com>
2023-03-06 19:03:26 +0100

    env

--- config/config.js
+++ config/config.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV == 'production') {
+  require('dotenv').config();
+    console.log(process.env.MONGOURI?'mongo env':'mongo not found');
+}
+
 module.exports = {
     mongoURI:process.env.MONGOURI
 };
