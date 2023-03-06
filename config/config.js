c26d748b6bae12b6ef578710ba23fa46c90ab021
joelforworks <101220133+joelforworks@users.noreply.github.com>
2023-03-06 23:47:15 +0100

    add pdf

--- config/config.js
+++ config/config.js
@@ -1,7 +1,4 @@
-if (process.env.NODE_ENV == 'production') {
     require('dotenv').config();
-    console.log(process.env.MONGOURI?`mongo set ...${process.env.MONGOURI.slice(-5)}`:'mongo not found');
-}
 
 module.exports = {
     mongoURI:process.env.MONGOURI
