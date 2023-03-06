5ed712bf1a85d4d694b09cc3a8af7509e9779daf
joelforworks <101220133+joelforworks@users.noreply.github.com>
2023-03-06 18:56:22 +0100

    env

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 if (process.env.NODE_ENV == 'production') {
   require('dotenv').config();
-  console.log(process.env);
-    console.log("MONGO URI",process.env.MONGOURI);
 }
 
 const express = require('express')
